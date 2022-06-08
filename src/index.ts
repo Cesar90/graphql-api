49412a43773cf93f7b4bebc0cbb77904774cd6d4
Cesar <ccordero.007@gmail.com>
2022-06-08 17:38:18 -0600

    Database connection issue

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,56 @@
 import { createServer } from "http";
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
-import {
-  ApolloServerPluginLandingPageGraphQLPlayground
-} from "apollo-server-core";
+// import {
+//   ApolloServerPluginLandingPageGraphQLPlayground
+// } from "apollo-server-core";
 import dotenv from 'dotenv'
 dotenv.config()
 import { connectDatabase } from './database'
+import { typeDefs, resolvers } from './graphql'
 
 const startServer = async () => {
   const db = await connectDatabase()
   const app = express()
-  const httpServer = createServer(app)
+  const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) });
+  server.applyMiddleware({ app, path: '/api' })
+  app.listen(process.env.PORT)
+  console.log(`[app]:http://localhost:${process.env.PORT}`)
 
-  const typeDefs = gql`
-    type Query {
-      hello: String
-    }
-  `;
 
-  const resolvers = {
-    Query: {
-      hello: () => 'Hello world!',
-    },
-  };
 
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [
-      ApolloServerPluginLandingPageGraphQLPlayground(),
-    ],
-  })
+  // const db = await connectDatabase()
+  // // console.log(db)
+  // const app = express()
+  // const httpServer = createServer(app)
 
-  await apolloServer.start()
-  apolloServer.applyMiddleware({
-      app,
-      path: '/api'
-  })
+  // // const typeDefs = gql`
+  // //   type Query {
+  // //     hello: String
+  // //   }
+  // // `;
+
+  // // const resolvers = {
+  // //   Query: {
+  // //     hello: () => 'Hello world!',
+  // //   },
+  // // };
+
+  // const apolloServer = new ApolloServer({
+  //   typeDefs,
+  //   resolvers,
+  //   context: () => {({ db })},
+  // })
+
+  // // await apolloServer.start()
+  // apolloServer.applyMiddleware({
+  //     app,
+  //     path: '/api'
+  // })
   
-  httpServer.listen({ port: process.env.PORT || 4000 }, () =>
-    console.log(`Server listening on localhost:4000${apolloServer.graphqlPath}`)
-  )
+  // httpServer.listen({ port: process.env.PORT || 4000 }, () =>
+  //   console.log(`Server listening on localhost:4000${apolloServer.graphqlPath}`)
+  // )
 }
 
 startServer()
